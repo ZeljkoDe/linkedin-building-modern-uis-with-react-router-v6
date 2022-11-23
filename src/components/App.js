272eb009251d928a043fd20556e39980f7a5a992
Zeljko Damnjanovic <z.damnjanovic@emakina.rs>
2022-11-23 10:05:17 +0100

    Routing Basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import logo from '../assets/red30-tech-logo.png';
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import Categories from './Categories';
+import About from './About';
+import Header from './Header';
 
 function App() {
 	return (
 		<div className='app'>
-			<header className='container'>
-				<img
-					className='logo'
-					src={logo}
-					alt='Red30 Tech logo'
-					title='Red30 Tech | Home'
+			<Header />
+
+			<Routes>
+				<Route
+					path='/'
+					element={<Home title='Welcome to Red30 Tech' />}
 				/>
-			</header>
+				<Route path='categories' element={<Categories />} />
+				<Route path='about' element={<About />} />
+			</Routes>
 
 			<footer className='container'>
 				&copy;2022 | <a href='https://red30tech.com/'>Red30 Tech</a>
